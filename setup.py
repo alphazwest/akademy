0497dde577c41a11f59f0cf6009b86ef889a071c
alphazwest <alphazwest@gmail.com>
2023-01-24 21:43:02 -0500

    update - version numbers in build files

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_readme():
 
 setup(
     name="akademy",
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/alphazwest/akademy",
     author="Zack West",
     author_email="alphazwest@gmail.com",
